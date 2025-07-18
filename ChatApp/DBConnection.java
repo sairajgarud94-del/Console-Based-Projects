1f3968c5033cd57efdfa83759bd7a9edd6ea8f4e
sairajgarud94-del <sairajgarud94@gmail.com>
2025-07-18 18:57:24 +0530

    Update DBConnection.java

--- ChatApp/DBConnection.java
+++ ChatApp/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:12345/chatdb";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "Tejal@123"; 
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
